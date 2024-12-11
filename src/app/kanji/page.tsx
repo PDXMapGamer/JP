5f41563c648d9886f08b2acd56f58950bdaf9bb2
Thomas Harvey <thomasharvey1415@gmail.com>
2024-12-11 22:06:21 +0000

    created an array of all joyo kanji seperated by grade

--- src/app/kanji/page.tsx
+++ src/app/kanji/page.tsx
@@ -1,4 +1,22 @@
-export default function Kanji() {
+export default async function Kanji() {
+  let gradedKanji = [];
+  for (let i = 1; i <= 8; i++) {
+    if (!(i == 7)) {
+      gradedKanji.push(await getKanjiByGrade(i));
+    }
+  }
+  console.log(gradedKanji);
+  async function getKanjiByGrade(grade: number) {
+    const fetchedData = await fetch(`https://kanjiapi.dev/v1/kanji/grade-${grade}`);
+    //   console.log(fetchedData);
+    const wrangledData = await fetchedData.json();
+    return {
+      kanjiArray: wrangledData,
+      length: wrangledData.length,
+    };
+  }
+  // const test = await getKanjiByGrade(1);
+  // console.log(test);
   return (
     <>
       <div>hi</div>
